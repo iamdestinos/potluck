b10fa97ea9faca692d327682af2b76ba9caca900
jimenezfede <ricojimenez89@gmail.com>
2022-09-09 17:20:11 -0400

    added webpack config and set it up

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: './client/src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'client/src/public/dist/'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['@babel/preset-env', '@babel/preset-react'] },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  mode: 'development',
+  watch: true,
+  devServer: {
+    contentBase: path.join(__dirname, 'client/src/public/'),
+    port: 3000,
+    publicPath: 'http://localhost:3000/dist/',
+    hotOnly: true,
+  },
+};
