c6e3bff192880dc1c14d1fdc8332458d8f918bd3
jimenezfede <ricojimenez89@gmail.com>
2022-09-10 18:53:36 -0400

    added auth0 and env

--- client/index.js
+++ client/index.js
@@ -5,12 +5,22 @@ import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
+
+const domain = process.env.AUTH0_DOMAIN;
+const clientId = process.env.AUTH0_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
-    <App />
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </BrowserRouter>,
 );
