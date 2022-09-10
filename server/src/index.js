56c472236aa8c6f7090ae9ea7b1462024f1a88df
eekbk <eekeric@gmail.com>
2022-09-09 19:49:33 -0500

    serve static html

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 /**
  * This is the file where we set up the server connection
@@ -7,8 +8,11 @@ const express = require('express');
 // set app equal to an invocation of express
 const app = express();
 const port = 3000;
+const staticPath = path.resolve(__dirname, '../../client/src/public');
 
+app.use(express.static(staticPath));
 // connect port
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Potluck is listening on port ${port}`);
 });
