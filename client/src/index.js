9f3f79b781d1b43cc3638b1df09165203c087a8b
wilsuriel03 <lacurly00@gmail.com>
2022-09-12 10:40:12 -0500

    refactor: move file into src & destructure environment vars

---
+++ client/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App';
+
+const { DOMAIN } = process.env;
+const { CLIENT_ID } = process.env;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <BrowserRouter>
+    <Auth0Provider
+      domain={DOMAIN}
+      clientId={CLIENT_ID}
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
+  </BrowserRouter>,
+);
