e5489e38bc5ff3d44b8aa2dd6a7290f3ec03c75c
eekbk <eekeric@gmail.com>
2022-09-13 16:09:38 -0500

    add: profile-card.component and hard-coded events

--- client/src/routes/profile/profile.component.jsx
+++ client/src/routes/profile/profile.component.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import ProfileCard from '../../components/profile-card/profile-card.component';
 
-const Profile = () => <h1>Profile component</h1>;
+const Profile = () => (
+  <>
+    <ProfileCard />
+    {/* <h2>Profile Name</h2> */}
+    <h4>🍲🍲🍲🍲🍲🍲</h4>
+    <ul>Event Entry</ul>
+    <ul>Event Entry</ul>
+    <ul>Event Entry</ul>
+    <ul>Event Entry</ul>
+    <ul>Event Entry</ul>
+  </>
+
+);
 
 export default Profile;
