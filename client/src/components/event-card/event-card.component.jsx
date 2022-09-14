05a4ac7618bfe80b41bd1494baadcf4a404b1e59
iamdestinos <iamdestinos@gmail.com>
2022-09-14 15:41:10 -0500

    update: hooked up sample data

--- client/src/components/event-card/event-card.component.jsx
+++ client/src/components/event-card/event-card.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import FoodForm from '../food-form/food-form.component.jsx';
+import eventExamples from '../../../../server/src/sample-data/fake-events';
 
 const EventCard = () => {
     return (
         <div>
             <h1>Event Card Component</h1>
-            <FoodForm foods={[{name: 'main dish', course: 'main'}, {name: 'side dish', course: 'side'}]}></FoodForm>
+            <FoodForm foods={eventExamples[0].foods}></FoodForm>
         </div>
     )
 };
