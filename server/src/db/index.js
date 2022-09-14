9348ee908bdebd68c47aad3ff696ac961e28f137
wilsuriel03 <lacurly00@gmail.com>
2022-09-13 23:04:28 -0500

    refactor: use connection to mongo atlas

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,5 +1,10 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
+const clc = require('cli-color');
 
-mongoose.connect('mongodb://mongo-db:27017/potluck')
-  .then(() => console.log('Connection successful'))
-  .catch((err) => console.log('Connection unsuccessful', err));
+const { DB_URI } = process.env;
+
+mongoose
+  .connect(DB_URI)
+  .then(() => console.log(clc.white.bgGreen('Connected to Potluck-db...')))
+  .catch((err) => console.log(clc.white.bgRed('Failed to connect to Potluck-db...', err)));
