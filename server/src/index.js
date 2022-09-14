1bbf2d84f120feee2c65d4d715ad772d86ce6289
wilsuriel03 <lacurly00@gmail.com>
2022-09-13 23:05:40 -0500

    refactor: import env variables for server creation

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
+const clc = require('cli-color');
+require('dotenv').config();
 require('./db');
 
-const port = 3000;
+const { PORT } = process.env;
 
 const app = express();
 const staticPath = path.resolve(__dirname, '../../client/src/public');
 
 app.use(express.static(staticPath));
 
-app.listen(port, () => console.log(`Potluck is running on port ${port}`));
+app.listen(PORT, () => console.log(clc.green.bgWhite(`Potluck is running on port ${PORT}...`)));
