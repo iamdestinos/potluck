17657c71ffda7d77ce33a7344c7c012055b7f9b0
eekbk <eekeric@gmail.com>
2022-09-13 22:24:06 -0500

    add: sample data for users and events

--- server/src/sample-data/fake-events.js
+++ server/src/sample-data/fake-events.js
@@ -1,21 +1,150 @@
 export default [
   {
-    time: 'FILL_ME_IN',
+    _id: 6,
+    time: '2022-09-25T12:00:00Z',
     location: {
-      address: 'FILL_ME_IN',
-      city: 'FILL_ME_IN',
-      state: 'FILL_ME_IN',
-      zip: 'FILL_ME_IN',
+      address: '347 Verret St',
+      city: 'New Orleans',
+      state: 'LA',
+      zip: '70114',
     },
     foods: [{
-      type: Schema.Types.ObjectId,
-      name: 'FILL_ME_IN',
-      course: 'FILL_ME_IN',
+      userId: 1,
+      name: 'BBQ Chicken',
+      course: 'main',
+      ref: 'User',
+    },
+    {
+      userId: 3,
+      name: 'Macaroni',
+      course: 'side',
+      ref: 'User',
+    },
+    {
+      userId: 2,
+      name: 'Veggie Burgers',
+      course: 'main',
+      ref: 'User',
+    },
+    {
+      userId: 4,
+      name: 'Enchiladas',
+      course: 'main',
       ref: 'User',
     }],
-    attending: [{
-      type: Schema.Types.ObjectId,
+    attending: {
+      userIds: [1, 2, 3, 4],
+    },
+  },
+  {
+    _id: 7,
+    time: '2022-09-24T12:00:00Z',
+    location: {
+      address: '700 Elysian Fields Ave',
+      city: 'New Orleans',
+      state: 'LA',
+      zip: '70117',
+    },
+    foods: [{
+      userId: 2,
+      name: '7 layer bean dip',
+      course: 'side',
+      ref: 'User',
+    },
+    {
+      userId: 3,
+      name: '239-bean Irish bean stew',
+      course: 'side',
+      ref: 'User',
+    },
+    {
+      userId: 4,
+      name: 'Bean soup',
+      course: 'side',
+      ref: 'User',
+    },
+    {
+      userId: 5,
+      name: 'can of beans',
+      course: 'side',
       ref: 'User',
     }],
+    attending: {
+      userIds: [2, 3, 4, 5],
+    },
+  },
+  {
+    _id: 8,
+    time: '2022-09-23T12:00:00Z',
+    location: {
+      address: '1 Canal St',
+      city: 'New Orleans',
+      state: 'LA',
+      zip: '70130',
+    },
+    foods: [{
+      userId: 3,
+      name: 'Pulled pork',
+      course: 'main',
+      ref: 'User',
+    },
+    {
+      userId: 4,
+      name: 'Pork chops',
+      course: 'main',
+      ref: 'User',
+    },
+    {
+      userId: 5,
+      name: 'whole pig',
+      course: 'main',
+      ref: 'User',
+    },
+    {
+      userId: 1,
+      name: 'pork ribs',
+      course: 'main',
+      ref: 'User',
+    }],
+    attending: {
+      userIds: [1, 5, 3, 4],
+    },
+  },
+  {
+    _id: 9,
+    time: '2022-09-22T12:00:00Z',
+    location: {
+      address: '500 De Armas St',
+      city: 'New Orleans',
+      state: 'LA',
+      zip: '70114',
+    },
+    foods: [{
+      userId: 4,
+      name: 'Potato Salad',
+      course: 'side',
+      ref: 'User',
+    },
+    {
+      userId: 5,
+      name: 'Green bean casserole',
+      course: 'side',
+      ref: 'User',
+    },
+    {
+      userId: 1,
+      name: 'Hamburgers',
+      course: 'main',
+      ref: 'User',
+    },
+    {
+      userId: 2,
+      name: 'Pringles',
+      course: 'side',
+      ref: 'User',
+    }],
+    attending: {
+      userIds: [1, 2, 5, 4],
+    },
   },
 ];
---
+++ server/src/sample-data/fake-users.js
@@ -0,0 +1,82 @@
+export default [
+  {
+    _id: 1,
+    email: 'eric@gmail.com',
+    given_name: 'Eric',
+    name: 'Eric Kuehnemann',
+    picture: 'https://source.unsplash.com/1600x900/?guitar',
+    events: [
+      {
+        eventIds: [6, 8, 9],
+        ref: 'Event',
+      },
+    ],
+    clout: 50,
+    awards: ['vegetarian master', 'sides ninja'],
+    foods: [{}],
+  },
+  {
+    _id: 2,
+    email: 'adonis@gmail.com',
+    given_name: 'Adonis',
+    name: 'Adonis Suriel',
+    picture: 'https://source.unsplash.com/1600x900/?guitar',
+    events: [
+      {
+        eventIds: [6, 7, 9],
+        ref: 'Event',
+      },
+    ],
+    clout: 100,
+    awards: ['meat master', 'mains maniac'],
+    foods: [{}],
+  },
+  {
+    _id: 3,
+    email: 'fede@gmail.com',
+    given_name: 'Fede',
+    name: 'Federico Jimenez',
+    picture: 'https://source.unsplash.com/1600x900/?guitar',
+    events: [
+      {
+        eventIds: [6, 7, 8],
+        ref: 'Event',
+      },
+    ],
+    clout: 250,
+    awards: ['world traveler', 'sides ninja'],
+    foods: [{}],
+  },
+  {
+    _id: 4,
+    email: 'john@gmail.com',
+    given_name: 'John',
+    name: 'John Hollister',
+    picture: 'https://source.unsplash.com/1600x900/?guitar',
+    events: [
+      {
+        eventIds: [6, 7, 8, 9],
+        ref: 'Event',
+      },
+    ],
+    clout: 50,
+    awards: ['meat master', 'mains maniac'],
+    foods: [{}],
+  },
+  {
+    _id: 5,
+    email: 'caity@gmail.com',
+    given_name: 'Caity',
+    name: 'Caity Opelka',
+    picture: 'https://source.unsplash.com/1600x900/?guitar',
+    events: [
+      {
+        eventIds: [7, 8, 9],
+        ref: 'Event',
+      },
+    ],
+    clout: 50,
+    awards: ['vegetarian master', 'sides ninja'],
+    foods: [{}],
+  },
+];
