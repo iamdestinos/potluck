925672fb55c335ef151c9d4c78ff5b806ce067dc
jimenezfede <ricojimenez89@gmail.com>
2022-09-14 17:16:52 -0400

    added get and post request for events

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 require('./db');
+const { User, Event } = require('./models/index');
 
 const port = 3000;
 
@@ -8,5 +9,17 @@ const app = express();
 const staticPath = path.resolve(__dirname, '../../client/src/public');
 
 app.use(express.static(staticPath));
+
+app.get('/event', (req, res) => {
+  Event.find({})
+    .then(() => res.sendStatus(200))
+    .catch(() => res.sendStatus(500));
+});
+
+app.post('/event', (req, res) => {
+  Event.create({})
+    .then(() => res.sendStatus(201))
+    .catch(() => res.sendStatus(500));
+});
 
 app.listen(port, () => console.log(`Potluck is running on port ${port}`));
