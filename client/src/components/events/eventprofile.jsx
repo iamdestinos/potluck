6c62e3cef15604b71151539eebb8e6316a55c228
jimenezfede <ricojimenez89@gmail.com>
2022-09-15 18:38:27 -0400

    added event profile component

---
+++ client/src/components/events/eventprofile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EventProfile = ({selectedEvent}) =>{
+  console.log(selectedEvent);
+  return (
+    <>
+    <h1>Event Profile</h1>
+    </>
+  )
+} 
+
+export default EventProfile;
