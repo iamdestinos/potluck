2840adc8886b061d590e6d4a0c944cd44e3273eb
eekbk <eekeric@gmail.com>
2022-09-16 17:18:52 -0500

    update: eslint

--- client/src/contexts/user.context.jsx
+++ client/src/contexts/user.context.jsx
@@ -6,6 +6,7 @@ export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => null,
 });
+
 export const UserProvider = ({ children }) => {
   const { isAuthenticated, user } = useAuth0();
   const [currentUser, setCurrentUser] = useState(null);
