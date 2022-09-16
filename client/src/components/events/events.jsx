ae325de1f311d0967f3d97ada8736753026ebfae
jimenezfede <ricojimenez89@gmail.com>
2022-09-16 10:03:48 -0400

    set up event to render one event

--- client/src/components/events/events.jsx
+++ client/src/components/events/events.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 
-
 const Events = ({ event, handleEvent }) => (
-  <>
-    <div className='event' onClick={() => handleEvent(event)}>{event.location.address}</div>
-  </>
+    <div onClick={() => handleEvent(event)}>{event.location.address}</div>
 );
 
 export default Events;
