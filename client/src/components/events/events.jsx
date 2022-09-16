80575f9c71c96c135d4b2f3ddbdd90125a1fa835
iamdestinos <iamdestinos@gmail.com>
2022-09-16 11:01:28 -0500

    update: changed properties to match live data

--- client/src/components/events/events.jsx
+++ client/src/components/events/events.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Events = ({ event, handleEvent }) => (
-    <div onClick={() => handleEvent(event)}>{event.location.address}</div>
+    <div onClick={() => handleEvent(event)}>{event.eventName}</div>
 );
 
 export default Events;
