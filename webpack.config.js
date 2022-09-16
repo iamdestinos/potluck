7e4328e07f7979d0521667c138e30cec1cc36451
jimenezfede <ricojimenez89@gmail.com>
2022-09-16 11:23:16 -0400

    deleted old devserver commented out lines

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   devtool: 'inline-source-map',
@@ -10,7 +9,6 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'client/src/public/dist/'),
     filename: '[name].js',
-    // clean: true,
   },
   module: {
     rules: [
@@ -30,27 +28,9 @@ module.exports = {
   },
   resolve: { extensions: ['*', '.js', '.jsx'] },
   mode: 'development',
-  // devServer: {
-  //   static: {
-  //     directory: path.resolve(__dirname, 'dist'),
-  //   },
-  //   port: 3000,
-  //   compress: true,
-  //   client: {
-  //     overlay: true,
-  //   },
-  //   open: true,
-  //   hot: true,
-  //   historyApiFallback: true,
-  // },
   plugins: [
     new Dotenv({
       systemvars: true,
     }),
-    // new HtmlWebpackPlugin({
-    //   title: 'potluck',
-    //   filename: 'index.html',
-    //   template: 'client/src/public/template.html',
-    // }),
   ],
 };
