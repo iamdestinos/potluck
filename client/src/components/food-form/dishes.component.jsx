62f957795f2f2429ddae1dc3e78c7ab7aee93e8f
iamdestinos <iamdestinos@gmail.com>
2022-09-16 16:02:47 -0500

    update: functionality to add food to event

--- client/src/components/food-form/dishes.component.jsx
+++ client/src/components/food-form/dishes.component.jsx
@@ -1,13 +1,31 @@
-import React, { useState } from 'react';
-
+import React, { useState, useContext } from 'react';
+import axios from 'axios';
 import Dish from './dish.component.jsx';
+import { UserContext } from '../../contexts/user.context';
 
 const Dishes = (props) => {
     const [foods, setFoods] = useState(props.foods);
     const [value, setValue] = useState('');
+    const { currentUser } = useContext(UserContext);
 
     const clickHandler = () => {
-        console.log('button clicked!', value);
+        //create food object to be passed
+        let newFood = {
+            name: value,
+            course: props.title === 'Main Dishes' ? 'main' : 'side',
+            userId: currentUser._id,
+        }
+        console.log('button clicked!', newFood);
+        //add new food
+        axios.put(`/event/${props.eventId}`, { food: newFood })
+            .then(result => {
+                console.log(result);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+            //need event identification
+            
     }
 
     const inputHandler = (e) => {
