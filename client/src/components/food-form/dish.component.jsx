a70936bcab362d6780d8f72f1bf4f72b7fd2394f
iamdestinos <iamdestinos@gmail.com>
2022-09-17 14:38:03 -0500

    update: disable buttons when deleting/editing

--- client/src/components/food-form/dish.component.jsx
+++ client/src/components/food-form/dish.component.jsx
@@ -6,34 +6,40 @@ const Dish = (props) => {
   const [food, setFood] = useState(props.food);
   const { currentUser } = useContext(UserContext);
   const bool = currentUser ? currentUser._id : null;
-  const style = bool === food.userId ? { fontWeight: 'bold' } : { fontWeight: 'normal' };
-  
+  const [style, setStyle] = useState(bool === food.userId ? { fontWeight: 'bold' } : { fontWeight: 'normal' });
+  const [loading, setLoad] = useState(false);
+
   const clickDelHandler = () => {
     if (bool === food.userId) {
+      setLoad(true);
       axios.delete(`/event/${props.eventId}`, { data: { food: food } })
         .then(result => {
+          setStyle({textDecoration: "line-through", fontWeight: 'bold'});
         })
         .catch(err => {
           console.error(err);
+          setLoad(false);
         });
     }
   };
 
   const clickEditHandler = () => {
-    console.log('edit button clicked!');
     const newName = prompt('Enter new food:');
-    console.log('new food:', newName);
     const newFood = {
       name: newName,
       course: food.course,
       userId: food.userId,
     };
+
+    setLoad(true);
     axios.put(`/event/update/${props.eventId}`, { food: food, newFood: newFood})
       .then(result => {
-        console.log(`item ${food.name} updated!`);
+        setFood(newFood);
+        setLoad(false);
       })
       .catch(err => {
         console.error(err);
+        setLoad(false);
       });
   };
 
@@ -41,8 +47,8 @@ const Dish = (props) => {
       {food.name}
       { bool === food.userId ?
         <span className='float-right justify-content-between'>
-          <button onClick={clickEditHandler}>Edit</button>
-          <button onClick={clickDelHandler}>Del</button>
+          <button onClick={clickEditHandler} disabled={loading}>Edit</button>
+          <button onClick={clickDelHandler} disabled={loading}>Del</button>
         </span>
         : ''
       }
