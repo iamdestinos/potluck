4a0dc249f4b46139b3459d0440bd583db7eaf836
eekbk <eekeric@gmail.com>
2022-09-16 23:44:55 -0500

    add: dynamic clout and Event component, remove: event profile

--- client/src/components/profile-card/profile-card.component.jsx
+++ client/src/components/profile-card/profile-card.component.jsx
@@ -1,11 +1,28 @@
 import React, { useContext, useState, useEffect } from 'react';
 // import { useAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 import { UserContext } from '../../contexts/user.context';
+import { EventsContext } from '../../contexts/events.context';
+import Events from '../events/events';
 import EventProfile from '../events/eventprofile-card';
 
 const ProfileCard = () => {
   const { currentUser } = useContext(UserContext);
+  const { events } = useContext(EventsContext);
+  const userEvents = events.filter((event) => event.attending.includes(currentUser._id));
+  console.log('events:\n', events);
+  console.log('userEvents:\n', userEvents);
+
+  // create a number
+  const cloutDivided = Math.floor(currentUser.clout / 10);
+  let emojiClout = '';
+  const emojiString = '🍉🌭🍗🍔🥗🍲🍤🫔🌽🥟😁😁😁😁😁';
+
+  for (let i = 0; i < cloutDivided; i++) {
+    emojiClout += emojiString[Math.floor(Math.random() * emojiString.length)];
+  }
+
   // const [userEvents, setUserEvents] = useState([]);
   // send a get request to the event database that returns the events the current user is attending
   // useEffect(() => {
@@ -24,12 +41,25 @@ const ProfileCard = () => {
   return (
     <>
       <div className="card text-bg-light mb-3" style={{ maxWidth: '18rem' }}>
-        <img className="card-img-top" src={currentUser.picture} alt={currentUser.name} />
+        <div className="text-center">
+          <img className="card-img-top rounded-circle" style={{ width: '200px' }} src={currentUser.picture} alt={currentUser.name} />
+        </div>
         <div className="card-body text-center">
           <h2>{currentUser.name}</h2>
           <p>{currentUser.email}</p>
-          <p>clout: 🍲🍲🍲🍲🍲🍲</p>
+          <p>
+            {`clout: ${emojiClout}`}
+          </p>
         </div>
+        <ul className="list-group list-group-flush">
+          {userEvents.map((event, i) => (
+            <Link key={event._id} to={`/eventprofile/${event._id}`}>
+              <li className="list-group-item">
+                <Events key={event._id} event={event} />
+              </li>
+            </Link>
+          ))}
+        </ul>
       </div>
       {/* {userEvents.map((event) => <EventProfile selectedEvent={event} />)} */}
       {/* <li className="list-group-item">Event Entry</li>
