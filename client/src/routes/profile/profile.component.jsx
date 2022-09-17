1ce2298a6260d519619119be3bed595b8fa9c507
eekbk <eekeric@gmail.com>
2022-09-17 11:22:47 -0500

    remove: unused get request and context

--- client/src/routes/profile/profile.component.jsx
+++ client/src/routes/profile/profile.component.jsx
@@ -1,35 +1,33 @@
 import React, { useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 import { UserContext } from '../../contexts/user.context';
-import { EventsContext } from '../../contexts/events.context';
 import ProfileCard from '../../components/profile-card/profile-card.component';
 import EventProfileCard from '../../components/events/eventprofile-card';
 
-const Profile = () => {
-  const { currentUser } = useContext(UserContext);
-  const [userEvents, setUserEvents] = useState([]);
-  // send a get request to the event database that returns the events the current user is attending
-  useEffect(() => {
-    axios.get(`/event/users/${currentUser._id}`)
-      .then(({ data }) => {
-        // console.log('Heres the events I found:\n', data);
-        setUserEvents(data);
-      })
-      .catch((err) => {
-        console.log('This is the error from the get request in userEvents:\n', err);
-      });
-  }, []);
+const Profile = () =>
+// const { currentUser } = useContext(UserContext);
+// const [userEvents, setUserEvents] = useState([]);
+// // send a get request to the event database that returns the events the current user is attending
+// useEffect(() => {
+//   axios.get(`/event/users/${currentUser._id}`)
+//     .then(({ data }) => {
+//       // console.log('Heres the events I found:\n', data);
+//       setUserEvents(data);
+//     })
+//     .catch((err) => {
+//       console.log('This is the error from the get request in userEvents:\n', err);
+//     });
+// }, []);
 
-  return (
+  (
     <>
       <div className="d-flex justify-content-around pt-5">
         <ProfileCard />
       </div>
       {/* <div className="d-flex justify-content-around pt-5">
-        {userEvents.map((event) => <EventProfileCard key={event._id} view="profile" selectedEvent={event} />)}
+        {userEvents.map((event) => <EventProfileCard selectedEvent={event} />)}
       </div> */}
     </>
   );
-};
 
 export default Profile;
