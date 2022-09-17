fb83d63d9ad49bba008114f4a9e22d99352678d4
eekbk <eekeric@gmail.com>
2022-09-17 17:07:34 -0500

    add: dynamic rendering to events

--- client/src/components/events/events.jsx
+++ client/src/components/events/events.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { UserContext } from '../../contexts/user.context';
 
-const Events = ({ event }) => (
-  <div>{event.eventName}</div>
-);
+const Events = ({ event, view }) => {
+  const { currentUser } = useContext(UserContext);
+  const { foods } = event;
+  let userFoods;
+  if (currentUser) {
+    userFoods = foods.filter((food) => food.userId === currentUser._id);
+  }
+
+  const renderView = () => {
+    if (view === 'profile') {
+      return (
+        <>
+          <p>You are bringing:</p>
+          <ul>
+            {userFoods.map((food) => <li key={`${food}${food.userId}`}><i>{food.name}</i></li>)}
+          </ul>
+        </>
+      );
+    }
+    return [];
+  };
+
+  return (
+    <>
+      <h3>{event.eventName}</h3>
+      {renderView()}
+    </>
+  );
+};
 
 export default Events;
