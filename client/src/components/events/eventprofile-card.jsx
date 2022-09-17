b34dedb5d5f53527a8364cfed86e7abe11d6f9de
jimenezfede <ricojimenez89@gmail.com>
2022-09-17 17:39:30 -0400

    destructured

--- client/src/components/events/eventprofile-card.jsx
+++ client/src/components/events/eventprofile-card.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import FoodForm from '../food-form/food-form.component';
 
-const EventProfileCard = ({ selectedEvent }) => (
+const EventProfileCard = ({ selectEvent: {eventName, foods, _id, attending} }) => (
   <div className="card text-bg-light mb-3" style={{ maxWidth: '80em' }}>
-    <h1>{selectedEvent.eventName}</h1>
-    <FoodForm foods={selectedEvent.foods} eventId={selectedEvent._id} />
-    <div className="attending">{`${selectedEvent.attending.length} people attending`}</div>
+    <h1>{eventName}</h1>
+    <FoodForm foods={foods} eventId={_id} />
+    <div className="attending">{`${attending.length} people attending`}</div>
   </div>
 );
 
