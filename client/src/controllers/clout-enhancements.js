f7b5b63b51c59c32426925837895fc6bb7a88cb4
eekbk <eekeric@gmail.com>
2022-09-17 12:15:22 -0500

    create: controller for clout enhancement

---
+++ client/src/controllers/clout-enhancements.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+// create a helper function to use to up the clout
+const cloutEnhancer = () => {
+  // send a put request to the current user's id to update their clout level
+};
