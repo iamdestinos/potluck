e33334fbfec432088a7bc6695d7ab8b8480df189
iamdestinos <iamdestinos@gmail.com>
2022-09-17 10:54:07 -0500

    fix: render event profile while not logged in

--- client/src/components/food-form/dish.component.jsx
+++ client/src/components/food-form/dish.component.jsx
@@ -1,12 +1,26 @@
 import React, { useState, useContext } from 'react';
 import { UserContext } from '../../contexts/user.context';
+import axios from 'axios';
 
 const Dish = (props) => {
   const [food, setFood] = useState(props.food);
   const { currentUser } = useContext(UserContext);
-  const style = currentUser._id === food.userId ? { fontWeight: 'bold' } : { fontWeight: 'normal' };
+  const style = currentUser ? currentUser._id : null === food.userId ? { fontWeight: 'bold' } : { fontWeight: 'normal' };
   
-  return <li style={ style }>{food.name}</li>;
+  const clickHandler = () => {
+    axios.delete(`/event/${props.eventId}`, { food: food })
+      .then(result => {
+        console.log(`${food.name} deleted!`);
+      })
+      .catch(err => {
+        console.log(`failed to delete ${food.name}`);
+      });
+  };
+
+  return <li style={ style }>
+      {food.name}
+      <button className='float-right' onClick={clickHandler}>Del</button>
+    </li>;
 }
 
 export default Dish;
