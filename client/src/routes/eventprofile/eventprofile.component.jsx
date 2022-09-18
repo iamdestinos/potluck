b4b29128931620f2d023180364339c9c63efef72
eekbk <eekeric@gmail.com>
2022-09-18 09:45:46 -0500

    update: make letsGo async, add updateEvents

--- client/src/routes/eventprofile/eventprofile.component.jsx
+++ client/src/routes/eventprofile/eventprofile.component.jsx
@@ -1,14 +1,14 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { EventsContext, testArrChanger } from '../../contexts/events.context';
+import { EventsContext } from '../../contexts/events.context';
 import { UserContext } from '../../contexts/user.context';
 import EventProfileCard from '../../components/events/eventprofile-card';
 import cloutEnhancer from '../../controllers/clout-enhancements';
 
 const EventProfile = () => {
   const { id } = useParams();
-  const { events } = useContext(EventsContext);
+  const { events, setEvents, testArr } = useContext(EventsContext);
   const { currentUser } = useContext(UserContext);
   const [going, setGo] = useState({ going: false });
   const foundEvent = events.find((event) => event._id === id);
@@ -21,33 +21,48 @@ const EventProfile = () => {
     }
   }, []);
 
-  const letsGo = () => {
+  // create a get request and setEvent context
+  const updateEvents = async () => {
+    try {
+      const { data } = await axios.get('/event');
+      await setEvents(data);
+    } catch (err) {
+      console.error('This is the error in the try/catch:\n', err);
+    }
+  };
+
+  const letsGo = async () => {
+    console.log('going.going:\n', going.going);
     if (currentUser) {
       if (going.going) {
-        axios.put(`/event/going/${foundEvent._id}`, { event: { $push: { attending: currentUser._id } } })
-          .then(() => {
-            setGo({ going: !going.going });
-            cloutEnhancer(currentUser._id, 3);
-            testArrChanger();
-          })
-          .catch((err) => console.log(err));
+        try {
+          await axios.put(`/event/going/${foundEvent._id}`, { event: { $push: { attending: currentUser._id } } });
+          setGo({ going: !going.going });
+          await cloutEnhancer(currentUser._id, 3);
+          await updateEvents();
+          testArr.push('a');
+        } catch (err) {
+          console.error('This is the error #421:\n', err);
+        }
       } else {
-        axios.put(`/event/going/${foundEvent._id}`, { event: { $pull: { attending: currentUser._id } } })
-          .then(() => {
-            setGo({ going: !going.going });
-            cloutEnhancer(currentUser._id, -3);
-            testArrChanger();
-          })
-          .catch((err) => console.log(err));
+        try {
+          await axios.put(`/event/going/${foundEvent._id}`, { event: { $pull: { attending: currentUser._id } } });
+          setGo({ going: !going.going });
+          await cloutEnhancer(currentUser._id, -3);
+          await updateEvents();
+        } catch (err) {
+          console.error('This is the error #BTH:\n', err);
+        }
       }
     }
   };
+
   return (
     <>
       <div className="d-flex justify-content-around pt-5">
         <EventProfileCard selectEvent={foundEvent} />
       </div>
-      <button onClick={letsGo}>{going.going ? 'I Wanna Go!' : 'I Don\'t Wanna Go!'}</button>
+      <button onClick={letsGo} type="button">{going.going ? 'I Wanna Go!' : 'I Don\'t Wanna Go!'}</button>
     </>
   );
 };
