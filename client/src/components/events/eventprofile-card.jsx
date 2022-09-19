200249f0f7784164b49a66dace25b465c97d087f
eekbk <eekeric@gmail.com>
2022-09-18 22:26:25 -0500

    update: styling

--- client/src/components/events/eventprofile-card.jsx
+++ client/src/components/events/eventprofile-card.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import FoodForm from '../food-form/food-form.component';
 
-const EventProfileCard = ({ selectEvent: {eventName, foods, _id, attending} }) => (
-  <div className="card text-bg-light mb-3" style={{ maxWidth: '80em' }}>
+const EventProfileCard = ({
+  selectEvent: {
+    eventName, foods, _id, attending,
+  },
+}) => (
+  <div className="card p-4 d-flex justify-content-between" style={{ maxWidth: '80em' }}>
     <h1>{eventName}</h1>
     <FoodForm foods={foods} eventId={_id} attending={attending} />
     <div className="attending">{`${attending.length} people attending`}</div>
