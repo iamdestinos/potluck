bff591052687058fc06c54ff99873d4331cca5d3
jimenezfede <ricojimenez89@gmail.com>
2022-09-18 21:52:47 -0400

    created route path to create event component

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,15 @@ import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 import Profile from './routes/profile/profile.component';
-import EventList from './components/events/eventlist';
 import EventProfile from './routes/eventprofile/eventprofile.component';
+import CreateEvent from './components/events/createEvent';
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Navigation />}>
       <Route index element={<Home />} />
+      <Route path="/create-event" element={<CreateEvent />} />
       <Route path="/profile" element={<Profile />} />
-      {/* <Route path="/eventlist" element={<EventList />} /> */}
       <Route path="/eventprofile/:id" element={<EventProfile />} />
     </Route>
   </Routes>
